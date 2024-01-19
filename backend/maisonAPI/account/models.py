578d152bd2d75aa4aa8f37ed930764fca379f771
hadifayazi <hadifayazi4@gmail.com>
2024-01-19 12:19:10 +0100

    creating user model

--- backend/maisonAPI/account/models.py
+++ backend/maisonAPI/account/models.py
@@ -1,27 +1,50 @@
 from typing import Any
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import UserManager, AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
 
 
 class CustomUserManager(UserManager):
-    """Custom Manager to handle creating of users"""
-
-    def _create_user(self, email, password, **extra_fields):
+    def _create_user(self, email: str, password: str | None = ..., **extra_fields):
         if not email:
-            raise ValueError('Email is required.')
-        email = self.normalize_email(email=email)
-        user = self.model(email,  **extra_fields)
+            raise ValueError("Email is requied.")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
         return user
 
-    def create_user(self, email: str | None = ..., password: str | None = ..., **extra_fields: Any) -> Any:
+    def create_user(self, email: str | None = ..., password: str | None = ..., **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(email, password, **extra_fields)
 
-    def creat_superuser(self, email: str | None = ..., password: str | None = ..., **extra_fields: Any) -> Any:
+    def create_superuser(self, email: str | None, password: str | None, **extra_fields: Any) -> Any:
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         return self._create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, blank=True, default='')
+    name = models.CharField(max_length=255, blank=True, default='')
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(blank=True, null=True)
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'User'
