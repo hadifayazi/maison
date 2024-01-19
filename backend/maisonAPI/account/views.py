f10e6dde4c199614aa8f0255ec364479553186f2
hadifayazi <hadifayazi4@gmail.com>
2024-01-19 14:44:38 +0100

    creating user create view

--- backend/maisonAPI/account/views.py
+++ backend/maisonAPI/account/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Register/create a new user."""
+    queryset = User.objects.none()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            if User.objects.filter(email=email).exists():
+                return Response({'error': 'User with this email already exists.'}, status=status.HTTP_400_BAD_REQUEST)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
