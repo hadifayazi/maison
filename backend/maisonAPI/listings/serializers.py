deae6d55d489d78aca2a817fa25bfe86451fffa2
hadifayazi <hadifayazi4@gmail.com>
2024-01-20 15:04:12 +0100

    serilizers for listing and imagelisting models

---
+++ backend/maisonAPI/listings/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Listing, ListingImage
+
+
+class ListingImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ListingImage
+        fields = ['image']
+
+
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = ['title', 'slug', 'city', 'zipcode', 'state', 'price',
+                  'sale_type', 'house_type', 'bedrooms', 'surface', 'images']
+
+
+class ListingDetailSerializer(serializers.ModelSerializer):
+    images = ListingImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Listing
+        fields = '__all__'
