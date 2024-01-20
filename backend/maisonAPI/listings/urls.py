219ea1afee8c346082996f5c5b290dcccc5c5dee
hadifayazi <hadifayazi4@gmail.com>
2024-01-20 16:03:29 +0100

    creating view to list and create listing

--- backend/maisonAPI/listings/urls.py
+++ backend/maisonAPI/listings/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-
+    path('', views.ListCreateListingView.as_view(), name='list-create-listing'),
 ]
--- backend/maisonAPI/listings/views.py
+++ backend/maisonAPI/listings/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .models import Listing, ListingImage
+from .serializers import ListingSerializer, ListingDetailSerializer, ListingImageSerializer
+from rest_framework import generics, permissions
 
-# Create your views here.
+
+class ListCreateListingView(generics.ListCreateAPIView):
+    """
+    View for listing creation and retrieval.
+
+    - For `GET` requests, retrieves a list of listings.
+    - For `POST` requests, creates a new listing.
+
+    Permissions:
+    - For `GET`: Allow any user (public access).
+    - For `POST`: Requires admin user.
+
+    Serializer:
+    - For `GET`: Uses `ListingSerializer`.
+    - For `POST`: Uses `ListingDetailSerializer`.
+    """
+    queryset = Listing.objects.all().order_by(
+        '-list_date').filter(is_published=True)
+    lookup_field = 'slug'
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return ListingDetailSerializer
+        return ListingSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [permissions.IsAdminUser()]
+        return [permissions.AllowAny()]
