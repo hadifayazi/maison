2c3b8bdc3aeb69ced3187c0b4fd78cc400b7de1e
hadifayazi <hadifayazi4@gmail.com>
2024-01-20 11:52:04 +0100

    adding list create reator view

--- backend/maisonAPI/realtors/views.py
+++ backend/maisonAPI/realtors/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Realtor
+from .serializers import RealtorSerializer
+from rest_framework import permissions, generics
 
-# Create your views here.
+
+class ListCreateRealtor(generics.ListCreateAPIView):
+    """API endpoint that allows the creation and listing of realtors."""
+    queryset = Realtor.objects.all()
+    serializer_class = RealtorSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [permissions.IsAdminUser()]
+        return [permissions.AllowAny()]
