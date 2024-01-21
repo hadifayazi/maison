5d784f9609078b9307652ba4e6fa8c908c06deac
hadifayazi <hadifayazi4@gmail.com>
2024-01-21 10:39:46 +0100

    creating contact model

--- backend/maisonAPI/contacts/models.py
+++ backend/maisonAPI/contacts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Contact(models.Model):
+    """Model to represent contact"""
+    name = models.CharFieldm(max_length=200)
+    email = models.EmailField(max_length=100)
+    subject = models.CharField(max_length=100)
+    message = models.TextField(blank=True)
+    contact_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return self.email
